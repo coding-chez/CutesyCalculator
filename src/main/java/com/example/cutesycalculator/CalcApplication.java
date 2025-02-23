68a09ebe52246cc46977e51809737eb96b25e454
coding-chez <chestinemaycabiso@gmail.com>
2025-02-23 10:57:25 +0800

    objectify

--- src/main/java/com/example/cutesycalculator/CalcApplication.java
+++ src/main/java/com/example/cutesycalculator/CalcApplication.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class CalcApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/calculatorcabiso/calculator-view.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/cutesycalculator/calc-view.fxml")));
 
         primaryStage.setTitle("Cutesy Calculator");
         primaryStage.setScene(new Scene(root));
